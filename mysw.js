c0488cc30a1bbd533bf0f96a96e464599736a71f
insibya <nathanmroy@yahoo.com>
2019-06-15 15:37:53 -0700

    cleaned update with quote split

--- mysw.js
+++ mysw.js
@@ -1,16 +1,6 @@
 let input_ia = document.getElementById("inputAnswerHere");
 let submit = document.getElementById("quote-button");
 
-
-
-// input_ia.addEventListener("blur",checkData);
-
-// function getInputData(){
-//     let data = [];
-//     data.push(input_ia.value);
-//     console.log(data);
-// }
-
 function checkData(){
     if(this.value === ""){
         this.input.add("alert");
@@ -18,18 +8,20 @@ function checkData(){
         console.log("empty!")
     }
     console.log(this.value);
-    
 }
 
-// function buildingStarWars(){
-//     let result = [];
-//     let videos = JSON.items;
-//        for(let i = 0; i < videos.length; i++){
-//           let clip = videos[i];
-//           result.push('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + clip.id.videoId + '_D0ZQPqeJkk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
-          
-          
-        
-    //       }
-    //       return result;
-    //    }
+function getQuote() {
+    fetch("http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote")
+        .then((resp) => resp.json())
+        .then(function(data) {
+            let quoteParts = data.starWarsQuote.split(' — ', 2);
+            console.log("this is the quote: ", quoteParts[0]);
+            console.log("this is the source: ", quoteParts[1]);
+    })
+        .catch(function (error) {
+        console.log("An error occurred");
+    });
+}
+
+
+getQuote();
