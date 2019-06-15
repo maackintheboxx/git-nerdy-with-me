51108a59a24a74ee0b097da4aeea0b9a6c7c67de
Mackenzie Frambes <mackenzieframbes@Mackenzies-MacBook-Air.local>
2019-06-15 15:18:51 -0700

    adding my modified files

--- mysw.js
+++ mysw.js
@@ -1,18 +1,35 @@
 let input_ia = document.getElementById("inputAnswerHere");
-let generate = document.getElementById("generate-button");
+let submit = document.getElementById("quote-button");
+
+
+
 // input_ia.addEventListener("blur",checkData);
 
-function getInputData(){
-    let data = [];
-    data.push(input_ia.value);
-    console.log(data);
-}
+// function getInputData(){
+//     let data = [];
+//     data.push(input_ia.value);
+//     console.log(data);
+// }
 
 function checkData(){
     if(this.value === ""){
-        this.classList.add("alert");
+        this.input.add("alert");
         this.placeholder = "Required";
         console.log("empty!")
     }
     console.log(this.value);
+    
 }
+
+// function buildingStarWars(){
+//     let result = [];
+//     let videos = JSON.items;
+//        for(let i = 0; i < videos.length; i++){
+//           let clip = videos[i];
+//           result.push('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + clip.id.videoId + '_D0ZQPqeJkk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+          
+          
+        
+    //       }
+    //       return result;
+    //    }
